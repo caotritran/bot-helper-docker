4a27bbe166a36c6fbd8460f8a62d7de85ac799f4
tritran <tri@emobi.biz>
2023-05-05 16:09:51 +0700

    add script deploy latest

--- manifest/run.sh
+++ manifest/run.sh
@@ -4,6 +4,8 @@ GIT_COMMITID=${1:-latest}
 
 sed -i "s/COMMIT_ID/$GIT_COMMITID/g" ./deployment.yaml
 
-echo "$GIT_COMMITID"
+echo "GIT_COMMITID: $GIT_COMMITID"
 
-echo "$CONTEXT - $NAMESPACE" 
+cat ./deployment.yaml | grep "image"
+
+kubectl config use-context $CONTEXT && kubectl apply -f deployment.yaml -n $NAMESPACE
