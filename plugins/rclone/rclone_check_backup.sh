c7a13fe35b15af92c2dc3d488070a71dfaf0e6e2
tritran <tri@emobi.biz>
2023-05-24 10:28:19 +0700

    fix syntax 4

--- plugins/rclone/rclone_check_backup.sh
+++ plugins/rclone/rclone_check_backup.sh
@@ -14,8 +14,8 @@ found=false
 
 for i in `rclone listremotes --config /data/rclone.conf`; do
     rclone ls $i --config /data/rclone.conf | grep $custom_domain > /dev/null 2>&1
-    if rclone ls $i --config /opt/rclone.conf | grep -q $custom_domain; then
-        rclone ls $i --config /opt/rclone.conf | grep $custom_domain
+    if rclone ls $i --config /data/rclone.conf | grep -q $custom_domain; then
+        rclone ls $i --config /data/rclone.conf | grep $custom_domain
         echo "Files backup exists at remote **$i**"
         echo "==================================="
         found=true
